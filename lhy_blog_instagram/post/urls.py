c7f90fad6febbeeb4f8adcbfa9e620544021a960
Kim JinSeok <jskim2code@gmail.com>
2017-10-24 20:39:11 +0900

    04.Post에 Comment추가하기   댓글 작성을 위한 view, url설정   post/post_list.html에 댓글 입력을 위한 Form구현과 댓글 목록 출현

--- lhy_blog_instagram/post/urls.py
+++ lhy_blog_instagram/post/urls.py
@@ -4,4 +4,6 @@ from post import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list' ),
+    url(r'^(?P<post_pk>\d+)/comment/create/$', views.comment_create,
+        name='comment_create'),
 ]
